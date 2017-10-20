23edce5aa5cfdfe524642ddfcdc205bd5203dd5a
Rajini Wijayawardana <rajiniw95@gmail.com>
2017-10-20 22:49:45 +0530

    Footer changed in Event Page

--- cuv/application/views/eventpage.php
+++ cuv/application/views/eventpage.php
@@ -30,6 +30,7 @@
     <![endif]-->
 
   </head>
+  <!--google font taken-->
   <style>
     @import url('https://fonts.googleapis.com/css?family=Lobster');
   </style>
@@ -212,13 +213,8 @@
       <!--main content end-->
       <div class="text-right">
             <div class="credits">
-                <!--
-                    All the links in the footer should remain intact.
-                    You can delete the links only if you purchased the pro version.
-                    Licensing information: https://bootstrapmade.com/license/
-                    Purchase the pro version form: https://bootstrapmade.com/buy/?theme=NiceAdmin
-                -->
-                <a href="https://bootstrapmade.com/free-business-bootstrap-themes-website-templates/">Business Bootstrap Themes</a> by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                
+                
             </div>
         </div>
   </section>
