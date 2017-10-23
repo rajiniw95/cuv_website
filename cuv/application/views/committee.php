4e307c3564e656b7dffe3ab85cf95b0ff362a4c6
Anush Indrajith <anushcs55@gmail.com>
2017-10-23 16:06:03 +0530

    did some changes-committe page

--- cuv/application/views/committee.php
+++ cuv/application/views/committee.php
@@ -2,54 +2,42 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
-    <meta name="author" content="GeeksLabs">
-    <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
-    <link rel="shortcut icon" href="img/favicon.png">
+
+
 
 
     <title>CUV|Committee</title>
+     <link rel="stylesheet" href="css/main.css" />
 
-    <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- bootstrap theme -->
-    <link href="css/bootstrap-theme.css" rel="stylesheet">
-    <!--external css-->
-    <!-- font icon -->
+     <link href="css/bootstrap.min.css" rel="stylesheet">
+
+     <link href="css/bootstrap-theme.css" rel="stylesheet">
+
     <link href="css/elegant-icons-style.css" rel="stylesheet" />
     <link href="css/font-awesome.min.css" rel="stylesheet" />
-    <!-- Custom styles -->
+
     <link href="css/style.css" rel="stylesheet">
     <link href="css/style-responsive.css" rel="stylesheet" />
 
-     <!--Custom Style sheet for tumbnails-->
+
     <link href="css/thumbnail-gallery.css" rel="stylesheet">
 
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 -->
-    <!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-      <script src="js/respond.min.js"></script>
-      <script src="js/lte-ie7.js"></script>
-    <![endif]-->
+
 
   </head>
-  <!--google font taken-->
+
   <style>
     @import url('https://fonts.googleapis.com/css?family=Lobster');
   </style>
 
   <body>
-  <!-- container section start -->
-  <section id="container" class="">
-      <!--header start-->
+
+
       <header class="header dark-bg">
 
-
-            <!--logo start-->
             <a href="index.html" class="logo">Colombo University<span class="lite">Volunteers</span></a>
-            <!--logo end-->
+
 
             <div class="nav search-row" id="top_menu">
 
@@ -59,84 +47,122 @@
 
             </div>
       </header>
-      <!--header end-->
 
-
-
-      <!--main content start-->
       <section id="main-content_nosidebar">
-          <section class="wrapper">
-          <div class="container">
+        <section id="two" class="wrapper style1 special">
 
-      <h1 class="my-4 text-center text-lg-left">Committee</h1><br>
-      <div id="committee" class="row container-fluid text-center">
+          <div class="inner">
+            <header>
+						<h2>COMMITTEE</h2>
+            </header>
 
-    <h4>Official Committee</h4>
-    <br>
+						<h3>Official Committe</h3>
+            <br><br>
+            
 
-    <div class="col-sm-1"></div>
-    <div class="col-sm-10">
+    <div class="flex flex-4">
+						<div class="box person">
+							<div class="image round">
+								<img src="img/committee/Dr.M.T.M.Mahees.jpg" alt="Person 1" />
+							</div>
+							<h3>Dr.M.T.M.Mahees</h3>
 
-        <!-- ROW 1 -->
-        <div class="row slideanim">
+						</div>
+						<div class="box person">
+							<div class="image round">
+								<img src="img/committee/Ms.Nirosha_Kulasekara.jpg" alt="Person 2" />
+							</div>
+							<h3>Ms.Nirosha Kulasekara</h3>
 
-            <div class="col-sm-3">
-                <img src="img/committee/Dr.M.T.M.Mahees.jpg" height="100" width="100">
-                <h4>Dr.M.T.M.Mahees</h4>
-                <!-- <p class="names">name</p> -->
-                <!-- <p>email</p> -->
-            </div>
+						</div>
+						<div class="box person">
+							<div class="image round">
+								<img src="img/committee/Ms.Duleeka_Jayawardhana.jpg" alt="Person 3" />
+							</div>
+							<h3>Ms.Duleeka Jayawardhana</h3>
 
-            <div class="col-sm-3">
-                <img src="img/committee/Ms.Nirosha_Kulasekara.jpg" height="100" width="100">
-                <h4>Ms.Nirosha Kulasekara</h4>
-                <!-- <p class="names">name</p> -->
-                <!-- <p>email</p> -->
-            </div>
+						</div>
+						<div class="box person">
+							<div class="image round">
+								<img src="img/committee/K.G.K.Anuradha.jpg" alt="Person 4" />
+							</div>
+							<h3>K.G.K.Anuradha</h3>
+							<p>Faculty of Science</p>
+						</div>
+					</div>
 
-            <div class="col-sm-3">
-                <img src="img/committee/Ms.Duleeka_Jayawardhana.jpg" height="100" width="100">
-                <h4>Ms.Duleeka Jayawardhana</h4>
-                <!-- <p class="names">name</p> -->
-                <!-- <p>email</p> -->
-            </div>
+        <h3>Faculty Leaders</h3>
 
-            <div class="col-sm-3">
-              <img src="img/committee/K.G.K.Anuradha.jpg" height="100" width="100">
-              <h4>K.G.K.Anuradha</h4>
-              <p class="names">Faculty of Science</p>
+        <br></br>
 
-                <!-- <p>email</p> -->
-            </div>
+              <div class="row slideanim">
+                  <div class="col-sm-1"></div>
+                  <div class="col-sm-10">
 
-        </div>
+                      <div class="col-sm-4">
+                        <div class="box person">
+                          <div class="image round">
+                            <img src="img/committee/Kulan_Sachinthana.jpg" alt="Person 1" />
+                          </div>
+                          <h3>Kulan Sachinthana</h3>
+                          <p>University of Colombo School of Computing</p>
+                        </div>
+                      </div>
 
-        <h4>Faculty Leaders</h4>
-        <!-- ROW 2 -->
-        <br><br>
+                      <div class="col-sm-4">
+                        <div class="box person">
+                          <div class="image round">
+                            <img src="img/committee/D.M.Sampath_Darshana_Lakmal.jpg" alt="Person 2" />
+                          </div>
+                          <h3>D.M.Sampath Darshana Lakshan</h3>
+                          <p>Faculty of Law</p>
+                        </div>
+                      </div>
+
+                      <div class="col-sm-4">
+                        <div class="box person">
+                          <div class="image round">
+                            <img src="img/committee/Poornima_Thathsarani.jpg" alt="Person 3" />
+                          </div>
+                          <h3>Poornima Thathsarani</h3>
+                          <p>Faculty of Management and Finance</p>
+                        </div>
+                      </div>
+                  </div>
+                </div>
         <div class="row slideanim">
             <div class="col-sm-1"></div>
             <div class="col-sm-10">
 
                 <div class="col-sm-4">
-                    <img src="img/committee/Kulan_Sachinthana.jpg" height="100" width="100">
-                    <h4>Kulan Sachinthana</h4>
-                    <p class="names">University of Colombo School of Computing</p>
-                    <!-- <p>email</p> -->
+                  <div class="box person">
+                    <div class="image round">
+                      <img src="img/committee/Naduni_Jayasinghe.jpg" alt="Person 1" />
+                    </div>
+                    <h3>Naduni</h3><h3> Jayasinghe</h3>
+
+                    <p>Faculty of Arts</p>
+                  </div>
                 </div>
 
                 <div class="col-sm-4">
-                  <img src="img/committee/D.M.Sampath_Darshana_Lakmal.jpg" height="100" width="100">
-                  <h4>D.M.Sampath Darshana Lakshan</h4>
-                  <p class="names">Faculty of Law</p>
-                    <!-- <p>email</p> -->
+                  <div class="box person">
+                    <div class="image round">
+                      <img src="img/committee/Rev.Bro.Dom_Yohan_Wickramasekara.jpg" alt="Person 2" />
+                    </div>
+                    <h3>Rev.Bro.Dom Yohan Wickramasekara</h3>
+                    <p>Faculty of Arts</p>
+                  </div>
                 </div>
 
                 <div class="col-sm-4">
-                    <img src="img/committee/Poornima_Thathsarani.jpg" height="100" width="100">
-                    <h4>Poornima Thathsarani</h4>
-                    <p class="names">Faculty of Management and Finance</p>
-                    <!-- <p>email</p> -->
+                  <div class="box person">
+                    <div class="image round">
+                      <img src="img/committee/P.V.Imalka_Madhuwanthi.jpg" alt="Person 3" />
+                    </div>
+                    <h3>P.V.Imalka Madhuwanthi</h3>
+                    <p>Faculty of Education</p>
+                  </div>
                 </div>
             </div>
 
@@ -144,112 +170,63 @@
 
         </div>
 
-        <!-- ROW 3 -->
-        <br><br>
+
         <div class="row slideanim">
             <div class="col-sm-1"></div>
             <div class="col-sm-10">
 
                 <div class="col-sm-4">
-                    <img src="img/committee/Naduni_Jayasinghe.jpg" height="100" width="100">
-                    <h4>Naduni Jayasinghe</h4>
-                    <p class="names">Faculty of Arts</p>
-                    <!-- <p>email</p> -->
+                  <div class="box person">
+                    <div class="image round">
+                      <img src="img/committee/M.D.Sujan_Nayanajith.jpg" alt="Person 1" />
+                    </div>
+                    <h3>M.D.Sujan</h3><h3>Nayanajith</h3>
+
+                    <p>Institute of Indigenous Medicine</p>
+                  </div>
                 </div>
 
                 <div class="col-sm-4">
-                  <img src="img/committee/Rev.Bro.Dom_Yohan_Wickramasekara.jpg" height="100" width="100">
-                  <h4>Rev.Bro.Dom Yohan Wickramasekara</h4>
-                  <p class="names">Faculty of Arts</p>
-                  <!-- <p>email</p> -->
+                  <div class="box person">
+                    <div class="image round">
+                      <img src="img/committee/Kasun_Kumarage.jpg" alt="Person 2" />
+                    </div>
+                    <h3>Kasun</h3><h3> Kumarage</h3>
+                    <p>Sri Palee Campus</p>
+                  </div>
                 </div>
 
                 <div class="col-sm-4">
-                    <img src="img/committee/P.V.Imalka_Madhuwanthi.jpg" height="100" width="100">
-                    <h4>P.V.Imalka Madhuwanthi</h4>
-                    <p class="names">Faculty of Education</p>
-                    <!-- <p>email</p> -->
+                  <div class="box person">
+                    <div class="image round">
+                      <img src="img/committee/K.G.Dilshan_Chameera_Karunarathna.jpg" alt="Person 3" />
+                    </div>
+                    <h3>K.G.Dilshan Chameera</h3>
+                    <p>Faculty of Medicine</p>
+                  </div>
                 </div>
-
-
             </div>
+
             <div class="col-sm-1"></div>
+
         </div>
-
-        <!-- ROW 4 -->
-        <br><br>
-        <div class="row slide-anim">
-            <div class="col-sm-1"></div>
-            <div class="col-sm-10">
-            <div class="col-sm-4">
-                <img src="img/committee/M.D.Sujan_Nayanajith.jpg" height="100" width="100">
-                <h4>M.D.Sujan Nayanajith</h4>
-                <p class="names">Institute of Indigenous Medicine</p>
-                <!-- <p>email</p> -->
-            </div>
-
-            <div class="col-sm-4">
-                <img src="img/committee/Kasun_Kumarage.jpg" height="100" width="100">
-                <h4>Kasun Kumarage</h4>
-                <p class="names">Sri Palee Campus</p>
-                <!-- <p>email</p> -->
-            </div>
-
-            <div class="col-sm-4">
-                <img src="img/committee/K.G.Dilshan_Chameera_Karunarathna.jpg" height="100" width="100">
-                <h4>K.G.Dilshan Chameera Karunarathna</h4>
-                <p class="names">Faculty of Medicine</p>
-                <!-- <p>email</p> -->
-            </div>
-
-            </div>
-            <div class="col-sm-1"></div>
-        </div>
-
-
-    </div>
-    <div class="col-sm-1"></div>
-    <br><br>
+      </div>
+  </div>
 </div>
-
-
-
-
-
-    </div>
-    <!-- /.container -->
-
-
-          </section>
-      </section>
-
-
-      <!--main content end-->
-      <div class="text-right">
-            <div class="credits">
-
-
-            </div>
-        </div>
   </section>
-  <!-- container section end -->
-    <!-- javascripts -->
+</section>
+
+
+
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <!-- nice scroll -->
+
     <script src="js/jquery.scrollTo.min.js"></script>
     <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-    <!-- jquery knob -->
+
     <script src="assets/jquery-knob/js/jquery.knob.js"></script>
-    <!--custome script for all page-->
+
     <script src="js/scripts.js"></script>
-
-  <script>
-
-      //knob
-      $(".knob").knob();
-
-  </script>
 
 
   </body>
