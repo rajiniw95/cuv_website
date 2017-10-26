d2dbf2eec631c6c469ae2114f5c12f447b0e1afc
Anush Indrajith <anushcs55@gmail.com>
2017-10-26 19:37:48 +0530

    add new aboutus page

--- cuv/application/views/aboutpage.php
+++ cuv/application/views/aboutpage.php
@@ -1,242 +1,212 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
-    <meta name="author" content="GeeksLabs">
-    <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
-    <link rel="shortcut icon" href="img/favicon.png">
-
+<html>
+	<head>
+		<title>Colombo Universiy Volunteers</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="css/landpage.css" />
+		<link rel="stylesheet" href="css/transitive.css" />
     <title>CUV|AboutCUV</title>
+	</head>
+	<body>
+			<header id="header" class="alt">
+				<div class="logo"><a href="index.html">Colombo University <span>Volunteers</span></a></div>
+				<a href="#menu">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+
+					<li><a href="http://localhost/cuv_website/cuv/gallery">GALLERY</a></li>
+					<li><a href="http://localhost/cuv_website/cuv/reviews">REVIEWS</a></li>
+          <li><a href="http://localhost/cuv_website/cuv/calendar">UPCOMING EVENTS</a></li>
+          <li><a href="http://localhost/cuv_website/cuv/committee">COMMITTEE</a></li>
+          <li><a href="http://localhost/cuv_website/cuv/contactus">CONTACT US</a></li>
+          <li><a href="http://localhost/cuv_website/cuv/">LOGOUT</a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+		<section id="banner">
+      <div class="inner">
+        <header>
+          <h1>Colombo University Volunteers</h1>
+          <p>~ Knowledge Volunteerism ~</p>
+        </header>
+        <a href="http://localhost/cuv_website/cuv/register" class="button big scrolly">Be a Volunteer</a>
+      </div>
+			</section>
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- Section -->
+					<section class="wrapper style1">
+						<div class="inner">
+							<!-- 2 Columns -->
+								<div class="flex flex-2">
+									<div class="col col1">
+										<div class="image round fit">
+											<a href="generic.html" class="link"><img src="img/landpage/pic01.jpg" alt="" /></a>
+										</div>
+									</div>
+									<div class="col col2">
+										<h2 class="section-heading">About Us</h2>
+										<div class="separetor"></div>
+										<p class="lead">
+											Our aim is to establish an all faculty volunteer group network that focuses on student welfare, their social and psychological issues as well as a support
+			                 group to delegate the work done by the student counselor's office and to needycommunity outside the university through knowledge of students.
+										 </p>
+                       <div class="separetor"></div>
+									</div>
+								</div>
+						</div>
+					</section>
 
 
-    <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- bootstrap theme -->
-    <link href="css/bootstrap-theme.css" rel="stylesheet">
-    <!--external css-->
-    <!-- font icon -->
-    <link href="css/elegant-icons-style.css" rel="stylesheet" />
-    <link href="css/font-awesome.min.css" rel="stylesheet" />
-     <!-- This is our Google Arvo font and Font-Awesome -->
-     <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
-    <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
-    <!-- full calendar css-->
-    <link href="assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css" rel="stylesheet" />
-  <link href="assets/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" />
-    <!-- easy pie chart-->
-    <link href="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen"/>
-    <!-- owl carousel -->
-    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
-  <link href="css/jquery-jvectormap-1.2.2.css" rel="stylesheet">
-    <!-- Custom styles -->
-  <link rel="stylesheet" href="css/fullcalendar.css">
-  <link href="css/widgets.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/style-responsive.css" rel="stylesheet" />
-  <link href="css/xcharts.min.css" rel=" stylesheet">
-  <link href="css/jquery-ui-1.10.4.min.css" rel="stylesheet">
-    <!-- =======================================================
-        Theme Name: NiceAdmin
-        Theme URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-        Author: BootstrapMade
-        Author URL: https://bootstrapmade.com
-    ======================================================= -->
-  </head>
-
- <!--Google Arvo font-->
-  <style>
-@import url('https://fonts.googleapis.com/css?family=Arvo');
-</style>
-
-  <body>
-  <!-- container section start -->
-  <section id="container" class="">
 
 
-      <!--header start-->
-      <header class="header dark-bg">
+
+					<section id="two" class="wrap style3">
+				<div class="inner">
+					<div id="flexgrid">
+           <br></br>
+					 <br></br>
+					 <br></br>
+					 <br></br>
+					</div>
+				</div>
+			</section>
 
 
-            <!--logo start-->
-            <a href="index.html" class="logo">Colombo University<span class="lite">Volunteers</span></a>
-            <!--logo end-->
-
-            <div class="nav search-row" id="top_menu">
-
-            </div>
-
-            <div class="top-nav notification-row">
-
-            </div>
-      </header>
-      <!--header end-->
-
-      <!--sidebar start-->
-      <aside>
-          <div id="sidebar"  class="nav-collapse ">
-              <!-- sidebar menu start-->
-              <ul class="sidebar-menu">
-                  <li class="active">
-                      <a class="" href="http://localhost/cuv_website/cuv/about">
-
-                          <span>ABOUT CUV</span>
-                      </a>
-                  </li>
-                  <li class="active">
-                      <a class="" href="http://localhost/cuv_website/cuv/gallery">
-
-                          <span>GALLERY</span>
-                      </a>
-                  </li>
-                  <li class="active">
-                      <a class="" href="http://localhost/cuv_website/cuv/calendar">
-
-                          <span>UPCOMING EVENTS</span>
-                      </a>
-                  </li>
-                  <li class="active">
-                      <a class="" href="http://localhost/cuv_website/cuv/reviews">
-
-                          <span>REVIEWS</span>
-                      </a>
-                  </li>
-                  <li class="active">
-                      <a class="" href="http://localhost/cuv_website/cuv/committee">
-
-                          <span>COMMITTEE</span>
-                      </a>
-                  </li>
-                  <li class="active">
-                      <a class="" href="http://localhost/cuv_website/cuv/contactus">
-
-                          <span>CONTACT US</span>
-                      </a>
-                  </li>
-                  <li class="active">
-                      <a class="" href="http://localhost/cuv_website/cuv/">
-
-                          <span>LOGOUT</span>
-                      </a>
-                  </li>
-              </ul>
-              <!-- sidebar menu end-->
-          </div>
-      </aside>
-      <!--sidebar end-->
-
-      <!--main content start-->
-      <section id="main-content">
-          <section class="wrapper">
-          <div class="row">
-                <div class="col-lg-12">
-                    <div class="jumbotron">
-      <div class="container">
-        <h1>About Colombo University Volunteers...</h1>
-        <h3>Our aim is to establish an all faculty volunteer group network that focuses on student welfare, their social and psychological issues as well as a support group to delegate the work done by the student counselor's office and to needy community outside the university through knowledge of students.</h3> <br>
 
 
-      <h1>Our Mission</h1>
-        <h3>Colombo University Volunteers are aiming at social integrating and supporting psycho-social work among university community and support sustainable social development activities of outside the community through knowledge based volunteerism.</h3><br>
-
-        <h1>Our Vision</h1>
-        <h3>'Mobilizing knowledge based volunteers of University of Colombo and supporting needy university and outside community'</h3><br>
-        </div>
 
 
-   <div class="container">
-        
-
-        <button class="btn btn-primary btn-lg btn-block" type="button" onclick="window.location.href='http://localhost/cuv_website/cuv/gallery'">View our Photo Gallery...</button>
-        </div>
-    </div>
 
 
-<br><br><br>
-    <div id="get-involved">
-      <div class="container homepage">
-        <h2>Get Involved</h2>
-        <hr>
-        <div class="row">
 
-          <div class="col-md-4">
-            <span class="fa-stack fa-4x">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-user fa-stack-1x fa-inverse"></i>
-            </span>
-            <h3>Register</h3>
-            <p><a class="btn btn-default" href="http://localhost/cuv_website/cuv/register" role="button"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+				<!-- Section -->
+					<section class="wrapper style2">
+						<div class="inner">
+							<div class="flex flex-2">
+								<div class="col col2">
+									<h2 class="section-heading">Our Mission</h2>
+									<div class="separetor"></div>
+									<p class="lead">
+                <b>Colombo University Volunteers</b> are aiming at social integrating and supporting psycho-social work among
+                university community and support sustainable social development activities of outside the community through
+                knowledge based volunteerism.
             </p>
-          </div>
+						<div class="separetor"></div>
 
-          <div class="col-md-4">
-            <span class="fa-stack fa-4x">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
-            </span>
-            <h3>Collaborate</h3>
-            <p><a class="btn btn-default" href="http://localhost/cuv_website/cuv/collaborate" role="button"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
-          </div>
+								</div>
+								<div class="col col1 first">
+									<div class="image round fit">
+										<a href="generic.html" class="link"><img src="img/landpage/mission.jpg" alt="" /></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
 
-          <div class="col-md-4">
-            <span class="fa-stack fa-4x">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-money fa-stack-1x fa-inverse"></i>
-            </span>
-            <h3>Donate</h3>
-            <p><a class="btn btn-default" href="http://localhost/cuv_website/cuv/donate" role="button"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>
-          </div>
+					<section class="wrapper style1">
+						<div class="inner">
+							<!-- 2 Columns -->
+								<div class="flex flex-2">
+									<div class="col col1">
+										<div class="image round fit">
+											<a href="generic.html" class="link"><img src="img/landpage/vision.jpg" alt="" /></a>
+										</div>
+									</div>
+									<div class="col col2">
+										<h2 class="section-heading">Our Vision</h2>
+										<div class="separetor"></div>
 
-        </div>
-      </div> <!-- /container -->
-    </div>
-                </div>
-            </div>
-             
-          </section>
-      </section>
-  </section>
-  <!-- container section start -->
+										<p class="lead">
+										<i>
+								         'Mobilizing knowledge based volunteers of University of Colombo and supporting needy university and outside
+							            community'
+							     </i>
+                     <div class="separetor"></div>
+									</div>
+								</div>
+						</div>
+					</section>
 
-    <!-- javascripts -->
-    <script src="js/jquery.js"></script>
-  <script src="js/jquery-ui-1.10.4.min.js"></script>
-    <script src="js/jquery-1.8.3.min.js"></script>
-    <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
-    <!-- bootstrap -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- nice scroll -->
-    <script src="js/jquery.scrollTo.min.js"></script>
-    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-    <!-- charts scripts -->
-    <script src="assets/jquery-knob/js/jquery.knob.js"></script>
-    <script src="js/jquery.sparkline.js" type="text/javascript"></script>
-    <script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
-    <script src="js/owl.carousel.js" ></script>
-    <!-- jQuery full calendar -->
-    <<script src="js/fullcalendar.min.js"></script> <!-- Full Google Calendar - Calendar -->
-  <script src="assets/fullcalendar/fullcalendar/fullcalendar.js"></script>
-    <!--script for this page only-->
-    <script src="js/calendar-custom.js"></script>
-  <script src="js/jquery.rateit.min.js"></script>
-    <!-- custom select -->
-    <script src="js/jquery.customSelect.min.js" ></script>
-  <script src="assets/chart-master/Chart.js"></script>
+				<!-- Section -->
+					<section class="wrapper style2">
+						<div class="inner">
+							<header class="align-center">
+								<h2>Why Volunteer With Us?</h2>
 
-    <!--custome script for all page-->
-    <script src="js/scripts.js"></script>
-    <!-- custom script for this page-->
-    <script src="js/sparkline-chart.js"></script>
-    <script src="js/easy-pie-chart.js"></script>
-  <script src="js/jquery-jvectormap-1.2.2.min.js"></script>
-  <script src="js/jquery-jvectormap-world-mill-en.js"></script>
-  <script src="js/xcharts.min.js"></script>
-  <script src="js/jquery.autosize.min.js"></script>
-  <script src="js/jquery.placeholder.min.js"></script>
-  <script src="js/gdp-data.js"></script>
-  <script src="js/morris.min.js"></script>
-  <script src="js/sparklines.js"></script>
-  <script src="js/charts.js"></script>
-  <script src="js/jquery.slimscroll.min.js"></script>
-  <script>
+							</header>
+							<div class="flex flex-3">
+								<div class="col align-center">
+									<div class="image round fit">
+										<img src="img/landpage/pic03.jpeg" alt="" />
+									</div>
+									<p><b>Sharing Knowledge Among Rural Schools</b></p>
+								</div>
+								<div class="col align-center">
+									<div class="image round fit">
+										<img src="img/landpage/pic05.jpeg" alt="" />
+									</div>
+									<p><b>Sharing Knowledge Among Rural Village</b></p>
+								</div>
+								<div class="col align-center">
+									<div class="image round fit">
+										<img src="img/landpage/pic04.jpg" alt="" />
+									</div>
+									<p><b>Sharing Knowledge Among Universities</b></p>
+								</div>
+
+							</div>
+						</div>
+					</section>
+					<section class="wrapper style1">
+						<div class="inner">
+
+							<div class="flex flex-3">
+								<div class="col align-center">
+									<h2>Join the movement</h2>
+									<p>Join a local group of service-minded men and women who are working to strengthen your community </p>
+
+								</div>
+								<div class="col align-center">
+
+
+									<a href="http://localhost/cuv_website/cuv/donate" class="button">Donate</a>
+								</div>
+								<div class="col align-center">
+									<a href="http://localhost/cuv_website/cuv/collaborate" class="button">Collaborate</a>
+								</div>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<!--<footer id="footer">
+				<div class="copyright">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-snapchat"><span class="label">Snapchat</span></a></li>
+					</ul>
+					<p>&copy; Untitled. All rights reserved. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.</p>
+				</div>
+			</footer>-->
+
+		<!-- Scripts -->
+			<script src="js/jquery.min.js"></script>
+			<script src="js/jquery.scrolly.min.js"></script>
+			<script src="js/jquery.scrollex.min.js"></script>
+			<script src="js/skel.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
+
+	</body>
+</html>
